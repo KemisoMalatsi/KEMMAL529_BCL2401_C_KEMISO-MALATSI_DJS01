baf804fe84f8ce0aac714a2bbeb2df9bb7001dfb
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-05-08 05:21:02 +0200

    Debigging:
    When I run my code it shows "calcNewVel is not defined". The reason for this is becouse I forgot to parse "velocityInKilometersPerHour" as an argument and left it as "acc" instead.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fuelBurnRatePerSecond = 0.5; // fuel burn rate (kg/s)
 
 const newDistance = distaceInKilometers + (velocityInKilometersPerHour * timeInSeconds) //calcultes new distance
 const remainingFuel = fuelBurnRatePerSecond * timeInSeconds //calculates remaining fuel
-const newVelocity = calcNewVel(acc, velocityInKilometersPerHour, timeInSeconds) //calculates new velocity based on acceleration
+const newVelocity = calcNewVel(accelarationInMetersPerSecondSquare, velocityInKilometersPerHour, timeInSeconds) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (velocityInKilometersPerHour, accelarationInMetersPerSecondSquare, timeInSeconds) => velocityInKilometersPerHour + (accelarationInMetersPerSecondSquare * timeInSeconds);
