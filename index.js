33aa9c6c500be04cb5925b5b00d10a30f3eecdf2
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-05-08 05:08:13 +0200

    Change "d2" to "newDistance". "vel2"  to "newVelocity". "rf" to "remainingFuel". This time I don't have to specify that fuel is in Kilograms becouse I already mentioned in the parameters. As developers we try not to repeat ourselves to avoid unecessary confusion.

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const fuelInKilograms = 5000; // remaining fuel (kg)
 const fuelBurnRatePerSecond = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = distaceInKilometers + (velocityInKilometersPerHour * timeInSeconds) //calcultes new distance
-const rf = fbr * timeInSeconds //calculates remaining fuel
-const vel2 = calcNewVel(acc, velocityInKilometersPerHour, timeInSeconds) //calculates new velocity based on acceleration
+const newDistance = distaceInKilometers + (velocityInKilometersPerHour * timeInSeconds) //calcultes new distance
+const remainingFuel = fuelBurnRatePerSecond * timeInSeconds //calculates remaining fuel
+const newVelocity = calcNewVel(acc, velocityInKilometersPerHour, timeInSeconds) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (velocityInKilometersPerHour, accelarationInMetersPerSecondSquare, timeInSeconds) => { 
   return velocityInKilometersPerHour + (accelarationInMetersPerSecondSquare * timeInSeconds)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
